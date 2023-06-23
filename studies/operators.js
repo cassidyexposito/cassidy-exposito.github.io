11cb6ab4aee4bedf4dfb04872042ee4bb849cb99
Cassidy Exposito <cassidyexposito@gmail.com>
2023-06-23 22:09:41 +0000

    operators draft

---
+++ studies/operators.js
@@ -0,0 +1,64 @@
+/**
+ * OPERATORS:
+ * An operator performs some operation on single or multiple operands (data value) 
+ * and produces a result. Operands are to the left and right of your operators. We have 6 different 
+ * types of operators in javascript.
+ * 
+ */
+
+//1. Assignment Operators //
+// =  // puts a value in a variable
+
+var test = 15
+
+// += // addition shortcut
+
+//  instead of 
+ test = test + 5
+//  we can write 
+test += 5 //to update the value of test to 20
+
+// -= // subtraction shortcut
+
+//  instead of 
+test = test - 5
+//  we can write 
+test -= 5 //to update the value of test to 10
+
+
+// *= // multiplication shortcut
+
+//  instead of 
+test = test * 5
+//  we can write 
+test *= 5 //to update the value of test to 75
+
+// /= //division shortcut
+
+/  instead of 
+test = test / 5
+//  we can write 
+test /= 5 //to update the value of test to 3
+
+
+// %= // modulus shortcut 
+
+/  instead of 
+test = test % 5
+//  we can write 
+test  %= 5 //to update the value of test to 0
+
+//2. ARITHMETIC OPERATORS
+
+// + : Addition operator (adds numbers together)
+// - : Subtraction operator (subtracts numbers together)
+// * : Multiplication operator (multiplies numbers together)
+// / : Division operator (multiplies numbers together)
+// % : Modulus (Modulo) (Returns the remainder of two numbers)
+// ** : Exponentiation Operator (a ** b = a^b)
+
+//3. COMPARISON OPERATORS //
+// Comparison operators will always return true or false, they return a boolean (bool).
+
+// == : equality operator, it checks if two values are the same (doesn't care about the data type)
+// === : strict equality operator, it checks if two values are the same and also the same data type.
